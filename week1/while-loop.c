53612f8e6d5e4ad607ea554394afc2aa51ca836b
Jiani Xing <tinynancy@foxmail.com>
2025-12-03 14:08:28 +0800

    add while-loop.c to practice while loop structure

---
+++ week1/while-loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 1;
+
+    printf("Counting from 1 to 10 using while loop:\n");
+
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;   // increment i
+    }
+
+    return 0;
+}
